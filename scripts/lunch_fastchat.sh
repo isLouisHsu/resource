02a9b7f50957a1b376656ca667b4013c3742e884
Louis Hsu <30061342+isLouisHsu@users.noreply.github.com>
2024-02-07 17:56:36 +0800

    Update lunch_fastchat.sh

--- scripts/lunch_fastchat.sh
+++ scripts/lunch_fastchat.sh
@@ -13,11 +13,11 @@ nohup python3 -m fastchat.serve.controller > output/controller.out &
 # nohup python3 -m fastchat.serve.vllm_worker --model-path ${model_path} > output/vllm_workers.out &
 
 # NOTE: 需要根据`model_name`选择对话模板，如果未指定或不匹配则使用默认模板（vicuna），因此使用qwen等模型时不匹配
-model_path="/opt/llm/qwen-14B-Chat/"
+model_path="/home/louishsu/NewDisk/Garage/weights/llm/Qwen-14B-Chat/"
 model_name="qwen-14b-chat"
 # model_path="/opt/llm/Yi-34B-Chat/"
 # model_name="yi-7b-chat"
-nohup python3 -m fastchat.serve.model_worker --model-path ${model_path} --seed 42 \
+nohup python3 -m fastchat.serve.model_worker --model-path ${model_path} --load-8bit --seed 42 \
     --host 0.0.0.0 --port 21002 --worker-address http://0.0.0.0:21002 > output/model_workers.out &
 # nohup python3 -m fastchat.serve.vllm_worker --model-path ${model_path} --seed 42 \
 #     --host 0.0.0.0 --port 21002 --worker-address http://0.0.0.0:21002 > output/model_workers.out &
@@ -33,4 +33,3 @@ nohup python3 -m fastchat.serve.openai_api_server \
     --host 0.0.0.0 --port 8000 > output/openai_api_server.out &
 
 # ps -ef | grep fastchat
- 
